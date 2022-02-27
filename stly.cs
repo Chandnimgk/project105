4aa73b87e3aa78b575aa9c6da3d5e3cc7a836001
Chandnimgk <96677492+Chandnimgk@users.noreply.github.com>
2022-02-27 20:07:25 +0530

    Add files via upload

---
+++ stly.cs
@@ -0,0 +1,21 @@
+body
+{
+background-color: blue;
+color: blueviolet;
+}
+.object
+{
+    color: brown;
+    font-size: medium;
+}
+#camera
+{
+    border: 2px dashed burlywood;
+    width:450px;
+    height:450px;
+}
+#result_object_name,#result_object_accuracy
+{
+ color:royalblue;
+ font-size: medium;
+}
